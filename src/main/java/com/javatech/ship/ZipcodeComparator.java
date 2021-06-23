dcea0e5b7233d01e745bafe5d68ba1e4205e73ca
Manpreet kaur <Manpreetrandhawa38@gmail.com>
2021-06-23 08:49:06 -0700

    Shipping-Project

---
+++ src/main/java/com/javatech/ship/ZipcodeComparator.java
@@ -0,0 +1,13 @@
+package com.javatech.ship;
+
+import java.util.Comparator;
+
+public class ZipcodeComparator implements Comparator<Zipcode> {
+	public int compare(Zipcode interval1, Zipcode interval2) {
+	    if (interval1.getLower_bound() > interval2.getLower_bound())
+	      return 1;
+	    else
+	      return -1;
+
+}
+}
